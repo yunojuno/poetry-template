78e220314d52871180bb9e56d3c43cc66064d1a7
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-07-12 22:15:58 +0100

    Update default configs

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.1.0"
+default_app_config = "my_app.apps.MyAppConfig"
--- tests/test_my_app.py
+++ tests/test_my_app.py
@@ -1,5 +1,2 @@
-from my_app import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
+def test_foo():
+    assert True
