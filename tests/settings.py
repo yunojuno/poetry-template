dbb0b8be4b5ebb784c2bf3d59b0462a138471a67
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-08-12 11:11:22 +0100

    Update configs and add pyupgrade, flake8-print to pre-commit

--- tests/settings.py
+++ tests/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
 
 STATIC_URL = "/static/"
 
-SECRET_KEY = "secret"
+SECRET_KEY = "secret"  # noqa: S105
 
 LOGGING = {
     "version": 1,
--- tests/test_my_app.py
+++ tests/test_my_app.py
@@ -1,4 +1,4 @@
-def test_import():
+def test_import() -> None:
     import my_app  # noqa: F401
 
     assert "Empty test to ensure pytest passes."
